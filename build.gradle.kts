58a62c138e3a6dabd36594f2398a272c3d0a6ef6
bigheadbird <wjz138139@163.com>
2023-09-22 16:06:43 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,5 @@ repositories {
 dependencies {
     implementation(group = "com.azure", name = "azure-ai-formrecognizer", version = "4.0.0")
     implementation(group = "com.google.code.gson", name = "gson", version = "2.7")
+    implementation(group = "com.microsoft.azure.cognitiveservices", name = "azure-cognitiveservices-computervision", version = "1.0.9-beta")
 }
-
-
